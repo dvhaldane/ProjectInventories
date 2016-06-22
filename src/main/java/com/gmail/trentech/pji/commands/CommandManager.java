9df419ca56fa764c7ec6cff652e63607f357dfb2
TrenTech@gmail.com <TrenTech@gmail.com>
2016-06-22 00:12:13 -0400

    *

--- src/main/java/com/gmail/trentech/pji/commands/CommandManager.java
+++ src/main/java/com/gmail/trentech/pji/commands/CommandManager.java
@@ -6,17 +6,42 @@ import org.spongepowered.api.text.Text;
 
 public class CommandManager {
 
-	public CommandSpec cmdTest = CommandSpec.builder().permission("pji.cmd.inventory.test").arguments(GenericArguments.optional(GenericArguments.string(Text.of("inv")))).executor(new CMDTest()).build();
+	public CommandSpec cmdTest = CommandSpec.builder()
+		    .permission("pji.cmd.inventory.test")    
+		    .arguments(GenericArguments.optional(GenericArguments.string(Text.of("inv"))))
+		    .executor(new CMDTest())
+		    .build();
+	
+	public CommandSpec cmdCreate = CommandSpec.builder()
+		    .permission("pji.cmd.inventory.create")    
+		    .arguments(GenericArguments.optional(GenericArguments.string(Text.of("inv"))))
+		    .executor(new CMDCreate())
+		    .build();
+	
+	public CommandSpec cmdDelete = CommandSpec.builder()
+		    .permission("pji.cmd.inventory.delete")
+		    .arguments(GenericArguments.optional(GenericArguments.remainingJoinedStrings(Text.of("inv"))))
+		    .executor(new CMDDelete())
+		    .build();
 
-	public CommandSpec cmdCreate = CommandSpec.builder().permission("pji.cmd.inventory.create").arguments(GenericArguments.optional(GenericArguments.string(Text.of("inv")))).executor(new CMDCreate()).build();
+	public CommandSpec cmdSet = CommandSpec.builder()
+		    .permission("pji.cmd.inventory.set")
+		    .arguments(GenericArguments.optional(GenericArguments.string(Text.of("world"))), GenericArguments.optional(GenericArguments.string(Text.of("inv"))))
+		    .executor(new CMDSet())
+		    .build();
 
-	public CommandSpec cmdDelete = CommandSpec.builder().permission("pji.cmd.inventory.delete").arguments(GenericArguments.optional(GenericArguments.remainingJoinedStrings(Text.of("inv")))).executor(new CMDDelete()).build();
-
-	public CommandSpec cmdSet = CommandSpec.builder().permission("pji.cmd.inventory.set").arguments(GenericArguments.optional(GenericArguments.string(Text.of("world"))), GenericArguments.optional(GenericArguments.string(Text.of("inv")))).executor(new CMDSet()).build();
-
-	public CommandSpec cmdList = CommandSpec.builder().permission("pji.cmd.inventory.list").executor(new CMDList()).build();
-
-	public CommandSpec cmdInventory = CommandSpec.builder().permission("pji.cmd.inventory")
-		// .child(cmdTest, "test", "t")
-		.child(cmdCreate, "create", "c").child(cmdDelete, "delete", "d").child(cmdSet, "set", "s").child(cmdList, "list", "l").executor(new CMDInventory()).build();
+	public CommandSpec cmdList = CommandSpec.builder()
+		    .permission("pji.cmd.inventory.list")
+		    .executor(new CMDList())
+		    .build();
+	
+	public CommandSpec cmdInventory = CommandSpec.builder()
+			.permission("pji.cmd.inventory")
+			//.child(cmdTest, "test", "t")
+			.child(cmdCreate, "create", "c")
+			.child(cmdDelete, "delete", "d")
+			.child(cmdSet, "set", "s")
+			.child(cmdList, "list", "l")
+			.executor(new CMDInventory())
+			.build();
 }
